541d35e92ec8dfb43272c718aa9a092d3b9e6fcb
Abiyath Rahman <abiyathrahman@mail.weber.edu>
2026-06-05 22:55:44 -0600

    updated various endpoints

--- backend/competition.js
+++ backend/competition.js
@@ -4,34 +4,50 @@ const router = express.Router();
 
 // Route to fetch competition data
 
-router.get('/competitions', async (req, res) => {
+router.get('/leagues', async (req, res) => {
     try {
-        const response = await axios.get('http://api.football-data.org/v4/competitions', {
+        const response = await axios.get('https://v3.football.api-sports.io/leagues', {
             headers: {
-                'X-Auth-Token': process.env.FOOTBALL_DATA_API_KEY
-            }
-        });
-        const count = response.data.count;
-        const names = response.data.competitions.map(competition => competition.name);
-        const codes = response.data.competitions.map(competition => competition.code);
-        res.json({ count, names, codes });
-    } catch (error) {
-        console.error('Error fetching competitions:', error);
-        res.status(500).json({ error: 'Failed to fetch competitions' });
-    }
+                'x-apisports-key': process.env.FOOTBALL_DATA_API_KEY
+            },
 
+        });
+        leagues = response.data.response.map(league => league.league.name);
+        res.json({ leagues });
+
+    } catch (error) {
+        console.error('Error fetching team data:', error);
+        res.status(500).json({ error: 'Failed to fetch team data' });
+    }
 });
 
-// Route to get a specific competetion by code
-router.get('/competitions/PD', async (req, res) => {
+// Route to get a specific competetion by country
+router.get('/leagues/:country', async (req, res) => {
     try {
-        const response = await axios.get('http://api.football-data.org/v4/competitions/PD/teams', {
+        const response = await axios.get(`https://v3.football.api-sports.io/leagues?country=${req.params.country}`, {
             headers: {
-                'X-Auth-Token': process.env.FOOTBALL_DATA_API_KEY
+                'x-apisports-key': process.env.FOOTBALL_DATA_API_KEY
             }
         });
-        //const teams = response.data.teams.map(team => team.name);
-        res.json({ response: response.data });
+        leagues = response.data.response.map(league => league.league.name);
+        res.json({ leagues });
+    } catch (error) {
+        console.error('Error fetching competition:', error);
+        res.status(500).json({ error: 'Failed to fetch competition' });
+    }
+});
+
+// Route to get a specific competition by name
+router.get('/leagues/name/:name', async (req, res) => {
+    try {
+        const response = await axios.get(`https://v3.football.api-sports.io/leagues?name=${req.params.name}`, {
+            headers: {
+                'x-apisports-key': process.env.FOOTBALL_DATA_API_KEY
+            }
+        });
+        leagues = response.data.response.map(league => league.league.name);
+        seasons = response.data.response.map(league => league.seasons.map(season => season.year)).flat();
+        res.json({ leagues, seasons });
     } catch (error) {
         console.error('Error fetching competition:', error);
         res.status(500).json({ error: 'Failed to fetch competition' });
--- backend/team.js
+++ backend/team.js
@@ -4,17 +4,15 @@ const axios = require('axios');
 
 // Route to get a specific team data
 
-router.get('/teams/PD/Barcelona', async (req, res) => {
+router.get('/teams/:name', async (req, res) => {
     try {
-        const response = await axios.get('http://api.football-data.org/v4/teams/81', {
+        const response = await axios.get(`https://v3.football.api-sports.io/teams?name=${req.params.name}`, {
             headers: {
-                'X-Auth-Token': process.env.FOOTBALL_DATA_API_KEY
-            }
+                'x-apisports-key': process.env.FOOTBALL_DATA_API_KEY
+            },
+
         });
-        const name = response.data.name;
-        const coach = response.data.coach.name;
-        const players = response.data.squad.map(player => player.name);
-        res.json({ name, coach, players });
+        res.json({ response: response.data });
     } catch (error) {
         console.error('Error fetching team data:', error);
         res.status(500).json({ error: 'Failed to fetch team data' });
