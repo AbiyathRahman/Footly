e24652a1dcf9f029204666c9e4c12af06b4472a5
Abiyath Rahman <abiyathrahman@mail.weber.edu>
2026-06-08 06:43:36 -0600

    added functionality to get matches for a team based on its name

--- backend/routes/match.js
+++ backend/routes/match.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const axios = require('axios');
 const db = require('../db/conn');
 
-// Route to get a match for a team
-
+// Route to get matches for a league
 router.get('/matches/:name', async (req, res) => {
     try {
+        // Check if league exists in the database or get league id from the API and store it in the database for future use
         const leagueName = req.params.name;
         const leagueInDb = await db.getDb().collection('leagues').findOne({ name: leagueName });
         let leagueID;
@@ -23,7 +23,7 @@ router.get('/matches/:name', async (req, res) => {
             leagueID = leagueInDb.leagueID;
         }
 
-
+        // Fetch matches for the league
         const response = await axios.get(`https://v3.football.api-sports.io/fixtures?league=${leagueID}&season=2024`, {
             headers: {
                 'x-apisports-key': process.env.FOOTBALL_DATA_API_KEY
@@ -37,6 +37,46 @@ router.get('/matches/:name', async (req, res) => {
     }
 
 
+});
+
+// Get matches for a specific team
+router.get('/matches/team/:name', async (req, res) => {
+    try {
+        const teamName = req.params.name;
+        const existingTeam = await db.getDb().collection('teams').findOne({ name: teamName });
+        let teamId;
+        if (!existingTeam) {
+            const teamResponse = await axios.get(`https://v3.football.api-sports.io/teams?search=${teamName}`, {
+                headers: {
+                    'x-apisports-key': process.env.FOOTBALL_DATA_API_KEY
+                }
+            });
+            teamId = teamResponse.data.response[0].team.id;
+            await db.getDb().collection('teams').insertOne({ name: teamName, teamId: teamId });
+
+        }
+        else {
+            teamId = existingTeam.teamId;
+        }
+
+        const matchesResponse = await axios.get(`https://v3.football.api-sports.io/fixtures?team=${teamId}&season=2024`, {
+            headers: {
+                'x-apisports-key': process.env.FOOTBALL_DATA_API_KEY
+            }
+        });
+
+        const matches = matchesResponse.data.response.map(match => ({
+            fixtureId: match.fixture.id,
+            date: match.fixture.date,
+            homeTeam: match.teams.home.name,
+            awayTeam: match.teams.away.name,
+            score: match.teams.home.name + ' - ' + match.score.fulltime.home + ' : ' + match.score.fulltime.away + ' - ' + match.teams.away.name,
+        }));
+        res.json({ response: matches });
+    } catch (error) {
+        console.error('Error fetching team data:', error);
+        res.status(500).json({ error: 'Failed to fetch team data' });
+    }
 });
 
 module.exports = router;    
