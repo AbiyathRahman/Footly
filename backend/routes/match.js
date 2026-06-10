3dc0b425c37bf6631f418c71ac6af27b6b383884
Abiyath Rahman <abiyathrahman@mail.weber.edu>
2026-06-09 21:16:43 -0600

    added functionality to get live matches

--- backend/routes/match.js
+++ backend/routes/match.js
@@ -3,6 +3,23 @@ const router = express.Router();
 const axios = require('axios');
 const db = require('../db/conn');
 
+// Get all matches live
+router.get('/matches/live', async (req, res) => {
+    try {
+        const response = await axios.get(`https://v3.football.api-sports.io/fixtures?live=all`, {
+            headers: {
+                'x-apisports-key': process.env.FOOTBALL_DATA_API_KEY
+            },
+
+        });
+
+        res.json({ response: response.data.response });
+    } catch (error) {
+        console.error('Error fetching live match data:', error);
+        res.status(500).json({ error: 'Failed to fetch live match data' });
+    }
+});
+
 // Route to get matches for a league
 router.get('/matches/:name', async (req, res) => {
     try {
